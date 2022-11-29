9ec8cfe1b88761011e56c049b696ae59f7ed9e66
David Abram <david@crocoder.dev>
2022-11-29 17:40:53 +0100

    feat: init repo

---
+++ packages/api/index.ts
@@ -0,0 +1,5 @@
+export type { AppRouter } from "./src/router/_app";
+export { appRouter } from "./src/router/_app";
+
+export { createContext } from "./src/context";
+export type { Context } from "./src/context";
