3a67a11692ae948cccd40ef0d6ffa2a874ae83d5
David Abram <david@crocoder.dev>
2022-11-30 21:35:15 +0100

    feat: add app

---
+++ apps/app/postcss.config.cjs
@@ -0,0 +1,1 @@
+module.exports = require("@crocoderdev/tailwind-config/postcss");
--- apps/app/src/pages/index.tsx
+++ apps/app/src/pages/index.tsx
@@ -10,14 +10,14 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>crocoder.dev landing page</title>
-        <meta name="description" content="crocoder.dev landing page" />
+        <title>crocoder.dev web app</title>
+        <meta name="description" content="crocoder.dev web app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-8">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            crocoder.dev landing page
+            crocoder.dev web app
           </h1>
           <AuthShowcase />
           <Button />
